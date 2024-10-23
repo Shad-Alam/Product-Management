7330791d9dbb8e36506074845470e3ea2efec12d
Shad-Alam <shad.19bd@gmail.com>
2024-10-23 12:31:14 +0600

    entity class updated

--- ProductManagement/src/main/java/com/shad/ProductManagement/Model/Product.java
+++ ProductManagement/src/main/java/com/shad/ProductManagement/Model/Product.java
@@ -1,4 +1,108 @@
 package com.shad.ProductManagement.Model;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ssd_product")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ssd_pro_id")
+    private Long id;
+    @Column(name = "ssd_pro_name")
+    private String name;
+    @Column(name = "ssd_pro_description")
+    private String description;
+    @Column(name = "ssd_pro_price")
+    private BigDecimal price;
+    @Column(name = "ssd_pro_stockQuantity")
+    private int stockQuantity;
+    @Column(name = "ssd_pro_category")
+    private String category;
+    @Column(name = "ssd_pro_createdAt")
+    private LocalDateTime createdAt;
+    @Column(name = "ssd_pro_updatedAt")
+    private LocalDateTime updatedAt;
+
+    public Product(){
+
+    }
+
+    public Product(Long id, String name, String description, BigDecimal price, int stockQuantity, String category, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+        this.category = category;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
